280d4c41f7db8559bf8a2b76e58e72802fbbdf9b
Brendan Le Foll <brendan.le.foll@intel.com>
2016-09-21 14:38:41 +0100

    mraa.c: add error if adding a MRAA_GENERIC_FIRMATA subplat and not supported
    Signed-off-by: Brendan Le Foll <brendan.le.foll@intel.com>

--- src/mraa.c
+++ src/mraa.c
@@ -1035,6 +1035,10 @@ mraa_add_subplatform(mraa_platform_t subplatformtype, const char* uart_dev)
         }
         syslog(LOG_NOTICE, "mraa: Failed to add firmata subplatform");
     }
+#else
+    if (subplatformtype == MRAA_GENERIC_FIRMATA) {
+        syslog(LOG_NOTICE, "mraa: Cannot add Firmata platform as support not compiled in");
+    }
 #endif
 
     return MRAA_ERROR_INVALID_PARAMETER;
